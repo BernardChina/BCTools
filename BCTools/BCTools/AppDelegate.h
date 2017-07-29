f5d4164ddd28fe670ce44d6b7bd791d9e86a9bae
BernardChina <1054462882@qq.com>
2017-07-29 14:22:52 +0800

    创建BCTools工程

---
+++ BCTools/BCTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BCTools
+//
+//  Created by 刘勇强 on 2017/7/29.
+//  Copyright © 2017年 刘勇强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BCTools/BCTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BCTools
+//
+//  Created by 刘勇强 on 2017/7/29.
+//  Copyright © 2017年 刘勇强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
